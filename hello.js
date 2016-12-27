7143cdc70dba63746006cdea4bf30a44d61215a8
liuyongwei <18347451907@163.com>
2016-12-27 15:02:13 +0800

    提交hello.js

---
+++ hello.js
@@ -0,0 +1,20 @@
+/**
+ * Created by liuyongwei on 2016/12/27.
+ */
+import React from 'react'
+class hello extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render () {
+    return (
+      <div></div>
+    );
+  }
+}
+hello.propTypes = {
+  
+};
+
+export default hello;
